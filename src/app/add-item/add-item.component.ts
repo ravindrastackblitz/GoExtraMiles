5726ae869146c2cadd2002387a40b2a535831af5
RAVI KRISHNA <RAVI KRISHNA@LAPTOP-K5BM17F2>
2024-05-29 19:36:26 +0530

    add item

--- src/app/add-item/add-item.component.ts
+++ src/app/add-item/add-item.component.ts
@@ -179,9 +179,7 @@ catalogModel:any;
 URLS:any =[];
 Names:any =[];
 Submit() {
-  setTimeout(()=>{
-    this.spinner= true;
-  },2000)
+
   if (this.Additemdetails.valid && this.selectedFile.length > 0) {
     
         this.catalogModel = {
